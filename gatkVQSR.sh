0afc026ca9ac5ccabd5dc6d786c2d2e8f3907dc2
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2020-03-18 14:28:49 +0100

    looks indel recalibration is also working

--- gatkVQSR.sh
+++ gatkVQSR.sh
@@ -19,26 +19,47 @@ singularity exec /data1/containers/sarek-latest.simg gatk VariantRecalibrator \
   --resource:omni,known=false,training=true,truth=false,prior=12.0 ${OMNI1000G} \
   --resource:1000G,known=false,training=true,truth=false,prior=10.0 ${PHASE11000G} \
   --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${DBSNP146} \
-   -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
-   -mode SNP \
-   -O ${VQSROUT}.VQSR.recal.vcf \
-   --tranches-file ${VQSROUT}.VQSR.tranches \
-   --rscript-file ${VQSROUT}.plots.R      # needs r-ggplot2 conda package
-
+  -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
+  -mode SNP \
+  -O ${VQSROUT}.VQSR.SNP.recal.vcf \
+  --tranches-file ${VQSROUT}.VQSR.SNP.tranches \
+  --rscript-file ${VQSROUT}.SNP.plots.R      # needs r-ggplot2 conda package
 
 singularity exec /data1/containers/sarek-latest.simg gatk ApplyVQSR \
   -R ${GRCH38} \
   -V $1 \
-  -O ${VQSROUT}.recalibrated.vcf.gz \
+  -O ${VQSROUT}.SNP.recalibrated.vcf.gz \
   --truth-sensitivity-filter-level 99.0 \
-  --tranches-file  ${VQSROUT}.VQSR.tranches \
-  --recal-file ${VQSROUT}.VQSR.recal.vcf \
+  --tranches-file  ${VQSROUT}.VQSR.SNP.tranches \
+  --recal-file ${VQSROUT}.VQSR.SNP.recal.vcf \
   -mode SNP
 
 
 echo "######################################################################"
-echo "                        SNP recalibration"
+echo "                        indel recalibration"
 echo "######################################################################"
 
+
+MILLSINDELS=${REFDIR}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+
+singularity exec /data1/containers/sarek-latest.simg gatk VariantRecalibrator \
+  -R ${GRCH38} \
+  -V ${VQSROUT}.SNP.recalibrated.vcf.gz \
+  --resource:mills,known=false,training=true,truth=true,prior=12.0 ${MILLSINDELS} \
+  --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${DBSNP146} \
+  -an DP -an FS -an QD -an SOR -an MQRankSum -an ReadPosRankSum \
+  -mode INDEL \
+  -O ${VQSROUT}.VQSR.INDEL.recal.vcf \
+  --tranches-file ${VQSROUT}.VQSR.INDEL.tranches \
+  --rscript-file ${VQSROUT}.INDEL.plots.R
+
+singularity exec /data1/containers/sarek-latest.simg gatk ApplyVQSR \
+  -R ${GRCH38} \
+  -V ${VQSROUT}.SNP.recalibrated.vcf.gz \
+  -O ${VQSROUT}.SNP.INDEL.recalibrated.vcf.gz \
+  --truth-sensitivity-filter-level 99.0 \
+  --tranches-file  ${VQSROUT}.VQSR.INDEL.tranches \
+  --recal-file ${VQSROUT}.VQSR.INDEL.recal.vcf \
+  -mode INDEL
 
 
