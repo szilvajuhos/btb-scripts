b002981fe3b950c0059ee9f103ae58ab4a7cef46
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2020-04-07 10:03:27 +0200

    script skeletons

---
+++ strelkaFilter/plot_iris_svc.py
@@ -0,0 +1,168 @@
+"""
+==================================================
+Plot different SVM classifiers in the iris dataset
+==================================================
+
+Comparison of different linear SVM classifiers on a 2D projection of the iris
+dataset. We only consider the first 2 features of this dataset:
+
+- Sepal length
+- Sepal width
+
+This example shows how to plot the decision surface for four SVM classifiers
+with different kernels.
+
+The linear models ``LinearSVC()`` and ``SVC(kernel='linear')`` yield slightly
+different decision boundaries. This can be a consequence of the following
+differences:
+
+- ``LinearSVC`` minimizes the squared hinge loss while ``SVC`` minimizes the
+  regular hinge loss.
+
+- ``LinearSVC`` uses the One-vs-All (also known as One-vs-Rest) multiclass
+  reduction while ``SVC`` uses the One-vs-One multiclass reduction.
+
+Both linear models have linear decision boundaries (intersecting hyperplanes)
+while the non-linear kernel models (polynomial or Gaussian RBF) have more
+flexible non-linear decision boundaries with shapes that depend on the kind of
+kernel and its parameters.
+
+.. NOTE:: while plotting the decision function of classifiers for toy 2D
+   datasets can help get an intuitive understanding of their respective
+   expressive power, be aware that those intuitions don't always generalize to
+   more realistic high-dimensional problems.
+
+"""
+print(__doc__)
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+def load_data(data_file_name):
+    """Loads data from data_file_name. 
+    Stolen shamelessly from scikit-learn datasets module (see sklearn/datasets/_base.py). 
+    Beware of simplifications.
+
+    Parameters
+    ----------
+    data_file_name : string
+        Name of csv file to be loaded from
+        module_path/data/data_file_name. For example 'wine_data.csv'.
+
+    Returns
+    -------
+    data : Numpy array
+        A 2D array with each row representing one sample and each column
+        representing the features of a given sample.
+
+    target : Numpy array
+        A 1D array holding target variables for all the samples in `data.
+        For example target[0] is the target varible for data[0].
+
+    target_names : Numpy array
+        A 1D array containing the names of the classifications. For example
+        target_names[0] is the name of the target[0] class.
+    """
+    with open(data_file_name) as csv_file:
+        data_file = csv.reader(csv_file)
+        temp = next(data_file)
+        n_samples = int(temp[0])
+        n_features = int(temp[1])
+        target_names = np.array(temp[2:])
+        data = np.empty((n_samples, n_features))
+        target = np.empty((n_samples,), dtype=np.int)
+
+        for i, ir in enumerate(data_file):
+            data[i] = np.asarray(ir[:-1], dtype=np.float64)
+            target[i] = np.asarray(ir[-1], dtype=np.int)
+
+    return data, target, target_names
+
+
+# import some data to play with
+data, target, target_names = load_data('iris.csv')
+# This "dictifaction" is done in the original scikit-learn by the Bunch class ( find site-packages/ -name "*.py"| xargs grep "class Bunch" )
+iris = {'data':data, 'target':target, 'target_names':target_names}
+# Take the first two features. We could avoid this by using a two-dim dataset
+#X = iris.data[:, :2]
+#y = iris.target
+
+# "data" is a numpy.ndarray that has funny properties
+# by this [:, :2] we are taking the first two columns
+X = data[:, :2]
+y = target
+
+# we create an instance of SVM and fit out data. We do not scale our
+# data since we want to plot the support vectors
+C = 1.0  # SVM regularization parameter
+models = (svm.SVC(kernel='linear', C=C),
+          svm.LinearSVC(C=C, max_iter=10000),
+          svm.SVC(kernel='rbf', gamma=0.7, C=C),
+          svm.SVC(kernel='poly', degree=3, gamma='auto', C=C))
+models = (clf.fit(X, y) for clf in models)
+
+# title for the plots
+titles = ('SVC with linear kernel',
+          'LinearSVC (linear kernel)',
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel')
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    plot_contours(ax, clf, xx, yy,
+                  cmap=plt.cm.coolwarm, alpha=0.8)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
+plt.show()
