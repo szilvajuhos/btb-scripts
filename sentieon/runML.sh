34ac32195399e89ca4af4d2d73f1b8e4e160f72c
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2020-04-21 10:21:04 +0200

    DNA modifications for Sentieon

--- sentieon/runML.sh
+++ sentieon/runML.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 # skeleton script to run TNScope with machine-learning 
 # $1 -- TUMOR BAM
 # $2 -- NORMAL BAM
@@ -11,27 +11,29 @@ module load sentieon
 module load samtools
 module load bcftools
 
+date
+
 REFERENCE=/data1/references/igenomes/Homo_sapiens/GATK/GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens_assembly38.fasta
 ML_MODEL=/data1/software/sentieon/SentieonTNscopeModel_GiAB_HighAF_LowFP-201711.05.model
-ML_THRESHOL=0.81
+ML_THRESHOLD=0.81
 
 TBASE=`basename $1`
 TDIR=`dirname $1`
 TUMOR_DEDUPED_BAM=$1
-TUMOR_RECAL_DATA_TABLE=${1%*bam}*recal.table
 # sample name
 TUMOR=`samtools view -H ${TUMOR_DEDUPED_BAM}| awk '/^@RG/{print $4}'| uniq| cut -b 4-`
+TUMOR_RECAL_DATA_TABLE=${TDIR}/${TUMOR}*recal.table
 
 NBASE=`basename $2`
 NDIR=`dirname $2`
 NORMAL_DEDUPED_BAM=$2
-NORMAL_RECAL_DATA_TABLE=${2%.bam}*recal.table
 # sample name
 NORMAL=`samtools view -H ${NORMAL_DEDUPED_BAM}| awk '/^@RG/{print $4}'| uniq| cut -b 4-`
+NORMAL_RECAL_DATA_TABLE=${NDIR}/${NORMAL}*recal.table
 
 TMP_VARIANT_VCF=${TUMOR}_TMP_VARIANT_VCF
 VARIANT_VCF=TNScope_${TUMOR}_vs_${NORMAL}_raw.vcf
-FILTER_VARIANT_VCF=${VARIANT_VCF%_raw*}_filtered.vcf
+FILTER_VARIANT_VCF=${VARIANT_VCF%_raw*}_filtered.vcf.gz
 
 sentieon driver -t 46 -r $REFERENCE \
   -i $TUMOR_DEDUPED_BAM -q $TUMOR_RECAL_DATA_TABLE \
@@ -46,4 +48,4 @@ sentieon driver -t 46 -r $REFERENCE --algo TNModelApply \
 
 bcftools filter -s "ML_FAIL" -i "INFO/ML_PROB > $ML_THRESHOLD" $VARIANT_VCF \
   -O z -m x -o $FILTER_VARIANT_VCF
-
+date
--- sentieon/runSentieon.sh
+++ sentieon/runSentieon.sh
@@ -5,4 +5,4 @@
 
 #nextflow run nf-core/sarek -r 2.5.2 -profile munin --input ../tsv/P2233_101T_P2233_120N.tsv --sentieon --tools DNAseq,DNAscope,TNscope,ASCAT,ControlFREEC,Mutect2,Manta,Strelka,HaplotypeCaller --skip_qc --pon /data1/PON/vcfs/BTB.PON.vcf.gz --pon_index /data1/PON/vcfs/BTB.PON.vcf.gz.tbi $1
 
-nextflow run nf-core/sarek -r 2.5.2 -profile munin --step variantcalling --input $INPUT --sentieon --tools TNscope --skip_qc $1
+nextflow run nf-core/sarek -r dev -profile munin --step mapping --input $INPUT --sentieon --skip_qc all $1
