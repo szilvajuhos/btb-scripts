b179313db44e2907b515eb6a79fb695d067075a9
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2020-06-10 14:40:17 +0200

    made it classy

--- heatmap/hm.py
+++ heatmap/hm.py
@@ -5,76 +5,66 @@ import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 from numpy import arange
+import click
+import os
 
-fig = plt.figure(figsize=(15,7))
+class MakeHeat:
+    def __init__(self,csv):
 
-ax1 = plt.subplot(1, 3, 1)
-# set colormap
-cm = matplotlib.cm.bwr
+        fig = plt.figure(figsize=(15,7))
 
-# generate data
-# X = np.random.random([6,6])
-X = np.genfromtxt('chr1.csv',delimiter=',')
-#
-# we are going to have a CSV file for each chromosome with sample names like
-#
-# Sample1,0,10,2,0,3,0,4,0
-# Sample2,0,10,2,0,3,0,4,0
-# Sample3,0,10,2,0,3,0,4,0
-#
-# So we have to delete the first column like
-X = np.delete(X,0,1)
-X = np.log(X)
+        ax1 = plt.subplot(1, 2, 1)
+        # set colormap
+        cm = matplotlib.cm.bwr
 
-# get pairwise distance matrix
-pmat = pdist(X, "euclidean")
-# do hiearchical clustering
-linkmat = linkage(pmat)
+        # generate data
+        X = np.genfromtxt(csv,delimiter=',')
+        #
+        # we are going to have a CSV file for each chromosome with sample names like
+        #
+        # Sample1,0,10,2,0,3,0,4,0
+        # Sample2,0,10,2,0,3,0,4,0
+        # Sample3,0,10,2,0,3,0,4,0
+        #
+        # So we have to delete the first column like
+        X = np.delete(X,0,1)
+        X = np.log(X)
+        X = X / X.max()
 
+        # get pairwise distance matrix
+        pmat = pdist(X, "euclidean")
+        # do hiearchical clustering
+        linkmat = linkage(pmat)
 
+        plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False) # labels along the bottom edge are off
+        plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False) # labels along the left edge are off
+        dendrogram(linkmat,orientation='left')
+        x0,x1 = ax1.get_xlim()
+        y0,y1 = ax1.get_ylim()
 
+        chr1 = plt.subplot(1, 2, 2)
+        Y = X
 
-plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False) # labels along the bottom edge are off
-plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False) # labels along the left edge are off
-dendrogram(linkmat,orientation='left')
-x0,x1 = ax1.get_xlim()
-y0,y1 = ax1.get_ylim()
-#ax1.set_aspect((x1-x0)/(y1-y0))
+        plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False) # labels along the bottom edge are off
+        plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False) # labels along the left edge are off
+        plt.pcolor(Y,cmap='bwr')
+        # switch off labels
+        chr1.set_xticklabels([])
+        chr1.set_yticklabels([])
+        plt.colorbar()
+        plt.subplots_adjust(wspace=0.0, hspace=0.0)
+        basename = os.path.basename(csv)
+        base = basename.split(".")[0]
+        plt.savefig(base + ".png")
+        print("Figure printed to "+ base + ".png")
+        plt.show()
 
-# add a colorbar to the first plot and immediately make it invisible
-#cb = plt.colorbar(ax=ax1)
-#cb.ax.set_visible(False)
+@click.command(context_settings = dict( help_option_names = ['-h', '--help'] ))
+@click.option('--csv',      '-c', type=str, help='CSV file to process', required=True)
 
-chr1 = plt.subplot(1, 3, 2)
-Y = X
-#Y = np.random.random([6,26])
+def printCSV(csv):
+    heatMap = MakeHeat(csv)
 
-plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False) # labels along the bottom edge are off
-plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False) # labels along the left edge are off
-#plt.xticks(arange(0.5, 7.5, 1))
-plt.pcolor(Y,cmap='bwr')
-# set a colorbar
-#cb = plt.colorbar(ax=chr1)
-# and disable
-#cb.ax.set_visible(False)
-# switch off labels
-chr1.set_xticklabels([])
-chr1.set_yticklabels([])
-
-chr2 = plt.subplot(1, 3, 3)
-plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False) # labels along the bottom edge are off
-plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False) # labels along the left edge are off
-Y = np.random.random([6,16])
-plt.pcolor(Y,cmap='bwr')
-# switch off labels
-chr2.set_xticklabels([])
-chr2.set_yticklabels([])
-# PLOT the colorbar for the last one
-plt.colorbar()
-
-
-plt.subplots_adjust(wspace=0.0, hspace=0.0)
-
-plt.show()
-
+if __name__ == "__main__":
+  printCSV()
 
